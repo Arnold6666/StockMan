8c459d2d584c82a372c251fab9cde3e4da1c76c6
Arnold6666 <a0919691671@gmail.com>
2023-05-12 16:00:24 +0800

    將gpt 由fetch改成 axios

---
+++ stockman/src/components/index.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Navbar from './child/navbar';
+
+export default function Index() {
+  return (
+    <Fragment>
+      <div>hello </div>
+    </Fragment>
+  )
+}
